856dc772e8f8a4b2c469977e771e6afd9c4785d6
Sergiok12 <sergioabd460@gmail.com>
2023-01-23 21:30:39 -0600

    Inicio

---
+++ lib/views/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const  Text("Lector Qr"),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const  [
+            Text("Ok")
+          ],
+        ),
+      ),
+    );
+  }
+}
