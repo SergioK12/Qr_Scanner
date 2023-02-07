d323311ed743f99bbfc4112aafe39016e1bc8f7e
SergioK12 <sergioabd460@gmail.com>
2023-02-07 03:32:47 +0100

    todo funcionando, refactor

--- lib/views/direcciones_page.dart
+++ lib/views/direcciones_page.dart
@@ -1,38 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../providers/scan_list_provide.dart';
+import '../widgets/scantiles.dart';
 
 class DireccionesPage extends StatelessWidget {
   const DireccionesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final scanlistprovider = Provider.of<ScanListProvider>(context);
-    final scans = scanlistprovider.scans;
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (context, index) => Dismissible(
-        key: UniqueKey(),
-        background: Container(
-          color: Colors.red,
-        ),
-        onDismissed: (direction) {
-          scanlistprovider.borrarscansbyid(scans[index].id!);
-        },
-        child: ListTile(
-          leading:
-              Icon(Icons.wifi, color: Theme.of(context).primaryColor),
-          title: Text(scans[index].valor.toString()),
-          subtitle: Text(scans[index].id.toString()),
-          trailing: const Icon(
-            Icons.keyboard_arrow_right,
-            color: Colors.amber,
-          ),
-          // ignore: avoid_print
-          onTap: () => print(scans[index].id.toString()),
-        ),
-      ),
-    );
+    return const ScanTiles(tipo: 'http');
   }
 }
---
+++ lib/widgets/scantiles.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/scan_list_provide.dart';
+
+class ScanTiles extends StatelessWidget {
+  final String tipo;
+  const ScanTiles({super.key, required this.tipo});
+
+  @override
+  Widget build(BuildContext context) {
+    final scanlistprovider = Provider.of<ScanListProvider>(context);
+    final scans = scanlistprovider.scans;
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder: (context, index) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.red,
+        ),
+        onDismissed: (direction) {
+          scanlistprovider.borrarscansbyid(scans[index].id!);
+        },
+        child: ListTile(
+          leading: Icon(
+            (tipo == 'http')
+             ?Icons.wifi
+             :Icons.maps_home_work,
+             color: Theme.of(context).primaryColor
+            ),
+          title: Text(scans[index].valor.toString()),
+          subtitle: Text(scans[index].id.toString()),
+          trailing: const Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.amber,
+          ),
+          // ignore: avoid_print
+          onTap: () => print(scans[index].id.toString()),
+        ),
+      ),
+    );
+  }
+}
